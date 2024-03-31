d03a60ed77e96931c492a35b0a347fc585e56fde
unknown <maaher1145@gmail.com>
2024-04-01 05:47:14 +0600

    Active issue fixed

--- src/components/FunFacts.jsx
+++ src/components/FunFacts.jsx
@@ -5,56 +5,57 @@ import CountUp from "react-countup";
 import ScrollTrigger from "react-scroll-trigger";
 
 const FunFacts = () => {
-  const [counters, setCounters] = useState([]);
-  const [counterOn, setCounterOn] = useState(false);
+	const [counters, setCounters] = useState([]);
+	const [counterOn, setCounterOn] = useState(false);
 
-  useEffect(() => {
-    const fetchCounters = async () => {
-      try {
-        const response = await axios.get(`${baseUrl}/counter`);
-        const data = response.data.data;
-        setCounters(data);
-      } catch (error) {
-        console.error("Error fetching feature data:", error);
-      }
-    };
+	useEffect(() => {
+		const fetchCounters = async () => {
+			try {
+				const response = await axios.get(`${baseUrl}/counter`);
+				const data = response.data.data;
+				const activeCounters = data.filter((counter) => counter._status === 1);
+				setCounters(activeCounters);
+			} catch (error) {
+				console.error("Error fetching feature data:", error);
+			}
+		};
 
-    fetchCounters();
-  }, []);
+		fetchCounters();
+	}, []);
 
-  return (
-    <div>
-      <div id="fun-facts" className="fun-facts section overlay">
-        <div className="container">
-          <div className="row">
-            {counters.map((counter, index) => (
-              <div key={index} className="col-lg-3 col-md-6 col-12">
-                <div className="single-fun">
-                  <i className="icofont-simple-smile" />
-                  <div className="content">
-                    <ScrollTrigger
-                      onEnter={() => setCounterOn(true)}
-                      onExit={() => setCounterOn(false)}
-                    >
-                      {counterOn && (
-                        <CountUp
-                          className="counter"
-                          end={counter._amount}
-                          duration={2.75}
-                        />
-                      )}
-                    </ScrollTrigger>
+	return (
+		<div>
+			<div id="fun-facts" className="fun-facts section overlay">
+				<div className="container">
+					<div className="row">
+						{counters.map((counter, index) => (
+							<div key={index} className="col-lg-3 col-md-6 col-12">
+								<div className="single-fun">
+									<i className="icofont-simple-smile" />
+									<div className="content">
+										<ScrollTrigger
+											onEnter={() => setCounterOn(true)}
+											onExit={() => setCounterOn(false)}
+										>
+											{counterOn && (
+												<CountUp
+													className="counter"
+													end={counter._amount}
+													duration={2.75}
+												/>
+											)}
+										</ScrollTrigger>
 
-                    <p>{counter._name}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+										<p>{counter._name}</p>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default FunFacts;
